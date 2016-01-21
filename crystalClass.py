a5da36f592329a12186160dd3897ede82cb35617
Johnathon <jmrackham@gmail.com>
2016-01-20 23:57:28 -0700

    update forcrystalClass

--- crystalClass.py
+++ crystalClass.py
@@ -1,24 +1,42 @@
-import numpy as np
 # This is a class containing information about a ternary
 # alloy crystal structure
+import numpy as np
+
 class Crystal:
-   # lattice parameters for parent species. Al, Ga, Ni is A,B,C respectively
-   # in angstrom. [a1, a2, a3]
+    # lattice parameters for parent species. Al, Ga, Ni is A,B,C respectively
+    # in angstrom. [a1, a2, a3]
     latticeAl = [4.04, 4.04, 4.04]
     latticeGa = [4.52, 7.66, 4.52]
     latticeNi = [3.52, 3.52, 3.52]
     
-    def __init__(self, A, B, C, a1in, a2in, a3in, positions):
-        self.conA = A 
-        self.conB = B
-        self.conC = C
+    # class constructor, takes num of atoms of species A, B, C, the three
+    # lattice vectors, and a list containing atomic positions
+    def __init__(self, num, concentrations, a1in, a2in, a3in, positions):
+        # member data for concentrations
+        self.structNum = num
+        self.conA = concentrations[0] 
+        self.conB = concentrations[1]
+        self.conC = concentrations[2]
+        self.numAtoms = A + B + C
         
-        #numpy arrays
+        #numpy arrays for lattice basis vectors
         self.a1 = a1in
         self.a2 = a2in
         self.a3 = a3in
         
+    # Generates the reciprocal lattice vectors
     def reciprocalVecs():
-        b1 = 2*np.pi*np.cross(a2,a3)/(np.dot(a1,np.cross(a2,a3))
-        b2 = 2*np.pi*np.cross(a3,a1)/(np.dot(a2,np.cross(a3,a1))
-        b3 = 2*np.pi*np.cross(a1,a2)/(np.dot(a3,np.cross(a1,a2))
+        b1 = 2*np.pi*np.cross(a2,a3)/(np.dot(a1,np.cross(a2,a3)))
+        b2 = 2*np.pi*np.cross(a3,a1)/(np.dot(a2,np.cross(a3,a1)))
+        b3 = 2*np.pi*np.cross(a1,a2)/(np.dot(a3,np.cross(a1,a2)))
+        return b1, b2, b3
+    
+    # Calculates the length of the FBZ in each of the 3 directions and divides
+    # the width by the interval parameter and returns kpoint mesh
+    def kpointMesh(density):
+        k1 = int(density * np.linalg.norm(a1))
+        k2 = int(density * np.linalg.norm(a2))
+        k3 = int(density * np.linalg.norm(a3))
+        return k1, k2, k3
+        
+        
